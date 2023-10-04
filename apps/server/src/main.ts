d36436ed7f00841affa7d50e797fe452c45fdbf4
Juan Cruz <marquezjuanchy@hotmail.com>
2023-10-03 22:40:27 -0300

    feat: :zap: docker & auth init

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = new ConfigService();
+
+  const config = new DocumentBuilder()
+    .setTitle('App')
+    .setDescription('The App API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters();
+  app.useGlobalInterceptors();
+  app.useGlobalPipes();
+
+  await app.listen(configService.get('SERVER_PORT') || 3000, () => {
+    console.log(`Listening on port ${configService.get('SERVER_PORT')}`);
+  });
 }
 bootstrap();
